e18a690b95210a4cd0fdf6097484fc363912c885
Jack Pincus <jwpincus@gmail.com>
2017-10-24 21:15:47 -0700

    api does all of the things

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -6,4 +6,21 @@ class Api::MessagesController < ApplicationController
     render json: @messages
   end
 
+  def update
+    if params[:printed] == 'true'
+      msg = Message.find(params[:id])
+      msg.update_column :printed?, true
+      msg.save
+      render json: {
+        status: :ok,
+        message: "Successfully marked message as printed",
+        }.to_json
+    else
+      render json: {
+        status: 204,
+        message: "Didn't do shit",
+      }.to_json
+    end
+  end
+
 end
--- spec/api/print_queue_spec.rb
+++ spec/api/print_queue_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 describe 'Api print queue', type: :api do
+  before(:each) do
+    @msg = Message.create(author: "jack", body: 'test', title: 'coder')
+  end
   it 'returns the unprinted messages'do
-    msg = Message.create(author: "jack", body: 'test', title: 'coder')
+
     response = get '/api/messages'
     body = JSON.parse(response.body).first
-    expect(body['body']).to eq(msg.body)
-    expect(body['author']).to eq(msg.author)
-    expect(body['title']).to eq(msg.title)
+    expect(body['body']).to eq(@msg.body)
+    expect(body['author']).to eq(@msg.author)
+    expect(body['title']).to eq(@msg.title)
+  end
+  it 'can mark messages as printed' do
+    response = put "/api/messages/#{@msg.id}?printed=true"
+    expect(response.status).to eq(200)
+    expect(Message.last.printed?).to be(true)
   end
 end
